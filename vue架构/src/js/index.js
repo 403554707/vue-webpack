c5b95ec0fdaaf4ffced913b758e6707c6277c2e5
403554707 <45195498+403554707@users.noreply.github.com>
2021-01-14 09:24:56 +0800

    Add files via upload

---
+++ vue架构/src/js/index.js
@@ -0,0 +1,10 @@
+// index.js
+import Vue from 'vue'
+import App from '../components/app.vue'
+
+const root = document.createElement('div')
+document.body.appendChild(root)
+
+new Vue({
+    render: (h) => h(App)
+}).$mount(root)
